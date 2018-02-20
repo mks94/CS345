b011ba3b10402079c73c9ec28ca3ac2fb2e3aa75
Mariah Sheehan <mks94@drexel.edu>
2018-02-19 22:29:44 -0500

    Basic 2d with spawning/movement

---
+++ BunkerDefender/Assets/scripts/Knight/KnightSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightSpawner : MonoBehaviour {
+    private float _spawnCoolDown = 0.0f;
+
+    public GameObject Knight;
+    public Vector3 SpawnPoint;
+    public float Speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (_spawnCoolDown <= 0f)
+        {
+            GameObject se = GameObject.Instantiate(Knight);
+            se.transform.position = SpawnPoint;
+            _spawnCoolDown = 5.0f;
+        }
+        else
+            _spawnCoolDown -= Time.deltaTime;
+
+    }
+}
---
+++ BunkerDefender/Assets/scripts/Path/PathFinder.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinder : MonoBehaviour {
+    private Node[] _nodes;
+    private Path path;
+    private int _currentNode;
+    private Vector3 _position;
+    private float _time;
+
+    //public GameObject Knight;
+    public float Speed;
+
+	// Use this for initialization
+	void Start () {
+        _nodes = GameObject.Find("Path").GetComponentsInChildren<Node>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (_currentNode == _nodes.Length)
+            Destroy(this.gameObject);
+
+        _position = _nodes[_currentNode].transform.position;
+        _time += Time.deltaTime * Speed;
+        if (this.transform.position != _position)
+            this.transform.position = Vector3.Lerp(this.transform.position, _position, _time);
+        else
+        {
+            _currentNode++;
+            _time = 0;
+        }
+
+    }
+}
