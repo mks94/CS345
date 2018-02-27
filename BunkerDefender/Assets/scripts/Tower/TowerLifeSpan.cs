1ab72b6225922c2ffec5ae7137654d38059e1b67
Mariah Sheehan <mks94@drexel.edu>
2018-02-27 17:29:53 -0500

    Tower life scipt

---
+++ BunkerDefender/Assets/scripts/Tower/TowerLifeSpan.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerLifeSpan : MonoBehaviour {
+    private static int lifeCount = 3;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "knight" || collision.gameObject.tag == "Knight")
+        {
+            lifeCount--;
+            if (lifeCount == 0)
+            {
+                Destroy(this.gameObject);
+                //Implement end game
+            }
+        }
+
+    }
+}
