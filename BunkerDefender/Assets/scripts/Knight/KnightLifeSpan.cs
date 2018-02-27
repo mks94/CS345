70422611b5f53265d2da10a03fb4db761195057c
Mariah Sheehan <mks94@drexel.edu>
2018-02-26 23:45:00 -0500

    Added some art, knight death, change weapons

---
+++ BunkerDefender/Assets/scripts/Knight/KnightLifeSpan.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightLifeSpan : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "bullet" || collision.gameObject.tag == "Bullet")
+        {
+            Destroy(this.gameObject);
+            Destroy(collision.gameObject);
+        }
+            
+    }
+}
---
+++ BunkerDefender/Assets/scripts/Tower/ChangeWeapon.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeWeapon : MonoBehaviour {
+    public GameObject Gun;
+    public GameObject Cannon;
+    public GameObject Bomb;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("1"))
+        {
+            Gun.SetActive(true);
+            Cannon.SetActive(false);
+            Bomb.SetActive(false);
+        }
+        else if (Input.GetKeyDown("2"))
+        {
+            Gun.SetActive(false);
+            Cannon.SetActive(true);
+            Bomb.SetActive(false);
+        }
+        else if (Input.GetKeyDown("3"))
+        {
+            Gun.SetActive(false);
+            Cannon.SetActive(false);
+            Bomb.SetActive(true);
+        }
+
+    }
+}
