f8f60e52d7d5cb87b9a04d2da1534967cabdfc42
Mariah Sheehan <mks94@drexel.edu>
2018-03-13 20:34:28 -0400

    Updated stuff

--- BunkerDefender/Assets/scripts/Knight/KnightLifeSpan.cs
+++ BunkerDefender/Assets/scripts/Knight/KnightLifeSpan.cs
@@ -16,11 +16,16 @@ public class KnightLifeSpan : MonoBehaviour {
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "bullet" || collision.gameObject.tag == "Bullet" || collision.gameObject.tag == "CannonBall")
+        if (collision.gameObject.tag == "bullet" || collision.gameObject.tag == "Bullet")
         {
             Destroy(this.gameObject);
             Destroy(collision.gameObject);
             TowerLifeSpan.HitKnight();
         } 
+        else if(collision.gameObject.tag == "CannonBall")
+        {
+            Destroy(this.gameObject);
+            TowerLifeSpan.HitKnight();
+        }
     }
 }
